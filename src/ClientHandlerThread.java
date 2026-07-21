4de5b346460d614261ca430879038111537e165f
Harsh-Kaul <hpkaul@arizona.edu>
2026-07-21 13:53:11 +0400

    Implementing Worker Threads and selecting server port

---
+++ src/ClientHandlerThread.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ClientHandlerThread implements Runnable {
+	
+	private final Socket connection;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private String Id;
+	
+	public ClientHandlerThread(Socket connection) {
+		this.connection = connection;
+	}
+
+	@Override
+	public void run() {
+		try {
+			output = new ObjectOutputStream(connection.getOutputStream());
+			output.flush();
+			input = new ObjectInputStream(connection.getInputStream());
+			
+			output.writeObject("ENTER ID");
+			this.Id = (String) input.readObject();
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
--- src/mainServer.java
+++ src/mainServer.java
@@ -1,4 +1,23 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class mainServer {
+	
+	private static final int PORT = 8080;
+	public static void main(String args[]) {
+		try {
+			ServerSocket mainServer = new ServerSocket(PORT);
+			System.out.println("SERVER RUNNING AT PORT 8080");
+			Socket connection = mainServer.accept();
+			
+			
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
