b055f6c344d5a03887b6e7f91036d33f732781a7
Harsh-Kaul <hpkaul@arizona.edu>
2026-08-18 21:02:04 +0400

    Modularizong code + Starting work on Camera

--- src/ClientHandlerThread.java
+++ src/ClientHandlerThread.java
@@ -10,6 +10,8 @@ import java.time.format.DateTimeFormatter;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+
+
 public class ClientHandlerThread implements Runnable {
 
 	private final Socket connection;
@@ -32,6 +34,10 @@ public class ClientHandlerThread implements Runnable {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public void openCamera() {
+		
 	}
 
 	public String TimeStamp() {
@@ -257,114 +263,146 @@ public class ClientHandlerThread implements Runnable {
 			String Choice = (String) input.readObject();
 			Choice = Choice.trim();
 			if (Choice.startsWith("@")) {
-				String[] msg = Choice.split(" ", 2);
-				String recip = msg[0].substring(1);
-				if (msg.length == 2) {
-					if (DBManager.getContacts(this.Id, recip)) {
-						ClientHandlerThread recipId = mainServer.serverClient.get(recip);
-						if (recipId != null) {
-							recipId.sendMessage(this, msg[1]);
-
-						} else {
-							output.writeObject("RECIPIENT OFFLINE : Saving Messages");
-
-							DBManager.InsertChatRow(this.Id, recip, msg[1], TimeStamp());
-						}
-					} else {
-						output.writeObject("RECIPIENT NOT IN YOUR CONTACTS LIST(Use cmd /add)");
-					}
-				} else {
-					output.writeObject("USAGE: @username message");
-				}
+				this.SenderCommand(Choice);
 			} else if (Choice.startsWith("/")) {
 				String[] msg = Choice.split(" ", 2);
 				String cmd = msg[0].substring(1);
 				if (cmd.toLowerCase().equals("details")) {
-					String[] Details = DBManager.getInfo(this.Id);
-					output.writeObject("DETAILS");
-					output.writeObject("Name: " + Details[0]);
-					output.writeObject("UUID: " + Details[1]);
+					this.getDetailsCommand();
 				} else if (cmd.toLowerCase().equals("delete")) {
-					output.writeObject("ENTER Password");
-					String password = (String) input.readObject();
-					String val = DBManager.Search(this.Id);
-
-					if (val != null && BCrypt.checkpw(password, val)) {
-						if (DBManager.Delete(this.Id)) {
-							output.writeObject("REMOVING USER");
-							output.writeObject("r--ShutDown--r");
-						} else {
-							output.writeObject("ERROR COULD NOT DELETE USER");
-
-						}
-					} else {
-						output.writeObject("INCORRECT PASSWORD");
-					}
-
+					this.deleteUserCommand();
 				} else if (cmd.toLowerCase().equals("add")) {
-					output.writeObject("ENTER ContactName");
-					String name = (String) input.readObject();
-					output.writeObject("ENTER UUID");
-					String UUID = (String) input.readObject();
-
-					boolean isAuth = DBManager.checkContact(name, UUID);
-
-					if (isAuth) {
-						output.writeObject("Contact Added");
-						DBManager.addContact(this.Id, name);
-					} else {
-						output.writeObject("no contact");
-					}
+					this.AddUserCommand();
 				} else if (cmd.toLowerCase().startsWith("contacts")) {
-
-					List<String> contacts = DBManager.searchContactList(this.Id);
-					int i = 0;
-					if (contacts != null) {
-						if (contacts.size() != 0) {
-							for (String val : contacts) {
-								i++;
-								output.writeObject(i + ". " + val);
-							}
-						} else {
-							output.writeObject("No Contacts Found");
-						}
-					} else {
-						output.writeObject("Couldn't get contacts (maybe empty?).");
-					}
+					this.getContactsCommand();
 				} else if (cmd.toLowerCase().startsWith("history")) {
 					if (isHist) {
-						for (String[] Hist : History) {
-							output.writeObject("[" + Hist[3] + "] " + Hist[0] + " : " + Hist[2]);
-						}
-						DBManager.DeleteHistory(this.Id);
-						History = null;
-						isHist = false;
-					} else {
-						output.writeObject("You have No Messages");
+						isHist = this.getMessages(isHist, History);
 					}
+				}else if (cmd.toLowerCase().startsWith("call")) {
+						
+					
 				} else if (cmd.toLowerCase().startsWith("clear")) {
-					output.writeObject("r--Clear--r");
-					output.writeObject("WELCOME TO THE MAIN PAGE");
-					output.writeObject("(press help for commands or quit to exit)");
+					this.clearCommand();
 				}
 
 			} else if (Choice.toLowerCase().startsWith("help")) {
-
-				output.writeObject("Commands");
-				output.writeObject("@Username Message");
-				output.writeObject("/Details");
-				output.writeObject("/Add");
-				output.writeObject("/Contacts");
-				output.writeObject("/History");
-				output.writeObject("/Clear");
-				output.writeObject("/Delete");
-
+				this.getCommands();
 			} else {
 				output.writeObject("Incorrect format");
 			}
 
 		}
 
+	}
+	
+	private void SenderCommand(String Choice) throws IOException {
+		String[] msg = Choice.split(" ", 2);
+		String recip = msg[0].substring(1);
+		if (msg.length == 2) {
+			if (DBManager.getContacts(this.Id, recip)) {
+				ClientHandlerThread recipId = mainServer.serverClient.get(recip);
+				if (recipId != null) {
+					recipId.sendMessage(this, msg[1]);
+
+				} else {
+					output.writeObject("RECIPIENT OFFLINE : Saving Messages");
+
+					DBManager.InsertChatRow(this.Id, recip, msg[1], TimeStamp());
+				}
+			} else {
+				output.writeObject("RECIPIENT NOT IN YOUR CONTACTS LIST(Use cmd /add)");
+			}
+		} else {
+			output.writeObject("USAGE: @username message");
+		}
+	}
+	
+	private void getDetailsCommand() throws IOException {
+		String[] Details = DBManager.getInfo(this.Id);
+		output.writeObject("DETAILS");
+		output.writeObject("Name: " + Details[0]);
+		output.writeObject("UUID: " + Details[1]);
+	}
+	
+	private void deleteUserCommand() throws IOException, ClassNotFoundException {
+		output.writeObject("ENTER Password");
+		String password = (String) input.readObject();
+		String val = DBManager.Search(this.Id);
+		if (val != null && BCrypt.checkpw(password, val)) {
+			if (DBManager.Delete(this.Id)) {
+				output.writeObject("REMOVING USER");
+				output.writeObject("r--ShutDown--r");
+			} else {
+				output.writeObject("ERROR COULD NOT DELETE USER");
+			}
+		} else {
+			output.writeObject("INCORRECT PASSWORD");
+		}
+	}
+	
+	private void AddUserCommand() throws IOException, ClassNotFoundException {
+		output.writeObject("ENTER ContactName");
+		String name = (String) input.readObject();
+		output.writeObject("ENTER UUID");
+		String UUID = (String) input.readObject();
+		boolean isAuth = DBManager.checkContact(name, UUID);
+		if (isAuth) {
+			output.writeObject("Contact Added");
+			DBManager.addContact(this.Id, name);
+		} else {
+			output.writeObject("no contact");
+		}
+	}
+	
+	private void getContactsCommand() throws IOException {
+		List<String> contacts = DBManager.searchContactList(this.Id);
+		int i = 0;
+		if (contacts != null) {
+			if (contacts.size() != 0) {
+				for (String val : contacts) {
+					i++;
+					output.writeObject(i + ". " + val);
+				}
+			} else {
+				output.writeObject("No Contacts Found");
+			}
+		} else {
+			output.writeObject("Couldn't get contacts (maybe empty?).");
+		}
+	}
+	
+	private boolean getMessages(boolean isHist,List<String[]>History) throws IOException {
+		if (isHist) {
+			for (String[] Hist : History) {
+				output.writeObject("[" + Hist[3] + "] " + Hist[0] + " : " + Hist[2]);
+			}
+			DBManager.DeleteHistory(this.Id);
+			History = null;
+			
+		} else {
+			output.writeObject("You have No Messages");
+			
+		}
+		return false;
+	}
+	
+	
+	private void getCommands() throws IOException {
+		output.writeObject("Commands");
+		output.writeObject("@Username Message");
+		output.writeObject("/Details");
+		output.writeObject("/Add");
+		output.writeObject("/Contacts");
+		output.writeObject("/History");
+		output.writeObject("/Clear");
+		output.writeObject("/Delete");
+	}
+	
+	private void clearCommand() throws IOException {
+		output.writeObject("r--Clear--r");
+		output.writeObject("WELCOME TO THE MAIN PAGE");
+		output.writeObject("(press help for commands or quit to exit)");
 	}
 
 }
